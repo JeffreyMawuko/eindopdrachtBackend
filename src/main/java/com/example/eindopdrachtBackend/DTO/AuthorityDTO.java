65eff6a20521f5c70d98092a6e3ef25143890b63
Jeffrey M. Wilson <Jeffrey M. Wilson@BG-GH0001>
2023-06-03 17:53:06 +0100

    Initial commit

---
+++ src/main/java/com/example/eindopdrachtBackend/DTO/AuthorityDTO.java
@@ -0,0 +1,7 @@
+
+INSERT INTO WallBracket (size, adjustable, name, price)
+        VALUES ('60 inch', 'no', 'Sony', 50.99);
+
+        INSERT INTO WallBracket (size, adjustable, name, price)
+        VALUES ('75 inch', 'no', 'Panasonic', 70.99);
+
